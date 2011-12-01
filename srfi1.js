b270466a5f6a40268f7e9d59d9df1325e1a426ed
Nick Zarczynski <jacktradespublic@gmail.com>
2011-12-01 08:22:40 -0600

    Changed license to BSD 3 clause

--- srfi1.js
+++ srfi1.js
@@ -3,23 +3,38 @@
 //
 // srfi1 is a 'just for fun' implementation of Scheme's SRFI-1 library
 //
-// srfi1 is free software: you can redistribute it and/or
-// modify it under the terms of the GNU Affero General Public
-// License version 3 as published by the Free Software Foundation.
+// Copyright (c) 2011, Nick Zarczynski
+// All rights reserved.
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Affero General Public License version 3 for more details.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
 //
-// You should have received a copy of the GNU Affero General Public
-// License version 3 along with this program. If not, see
-// <http://www.gnu.org/licenses/>.
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of Nick Zarczynski nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+// IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //___________________________________________________________________________//
 
-// Putting the "use strict" statement here will affect all other files that 
-// are concatenated with this code.  If this file is concatenated with other 
-// files and they suddenly start breaking, comment this out.  For me this is 
+// Putting the "use strict" statement here will affect all other files that
+// are concatenated with this code.  If this file is concatenated with other
+// files and they suddenly start breaking, comment this out.  For me this is
 // preferable to adding "use strict" to every function definition in this file.
 "use strict";
 
